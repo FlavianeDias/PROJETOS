bbde93a10f36ef70440bdbcd13a6c5279b71f57f
Flaviane Costa Dias <105251424+FlavianeDias@users.noreply.github.com>
2024-01-15 23:18:49 -0300

    continuação_projetowefix

--- PROJETO_WEFIX2/config_ban.php
+++ PROJETO_WEFIX2/config_ban.php
@@ -1,5 +1,13 @@
 <?php
 
-    if(isset($_POST))
+    $servidor="localhost";
+    $usuario="root";
+    $senha="";
+    $dbname="estoque";
+
+    $conexao=mysqli_connect($servidor, $usuario, $senha, $dbname);
+    if(!$conexao){
+        die("Houve um erro: ".mysqli_connect_error());
+    }
 
 ?>
