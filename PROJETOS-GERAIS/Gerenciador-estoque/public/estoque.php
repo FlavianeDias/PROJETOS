e252aa63353c503fe700e0a92827bd5e56826020
Flaviane Costa Dias <105251424+FlavianeDias@users.noreply.github.com>
2025-09-11 21:47:51 -0300

    Atts-ii

--- PROJETOS-GERAIS/Gerenciador-estoque/public/estoque.php
+++ PROJETOS-GERAIS/Gerenciador-estoque/public/estoque.php
@@ -14,7 +14,7 @@ $stmt = $produto->listar();
     <meta charset="UTF-8">
     <title>Controle de Estoque</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../assets/css/custom.css" rel="stylesheet">
+    <link href="/PROJETOS-GERAIS/Gerenciador-estoque/assets/css/custom.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
